33ca1dfdc0931d43927c907609a2197ce62f76f7
SpicyCrabKing <paul_pawelec@hotmail.com>
2023-09-16 14:55:04 -0400

    Overhaul - Theme, Content, Design
    Cleaned up the theme to colors I liked, updated resume, home, and contact sections to something more final. Project section has added in slides for each project card, still in progress.

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,99 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+let slideIndex = [1,1];
+
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
+let dotId = ["dots1", "dots2", "dots3", "dots4"]
+
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+
+// Next/previous controls
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+// Thumbnail image controls
+function currentSlide(n, no) {
+  showSlides(n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let slides = document.getElementsByClassName(slideId[no]);
+  let dots = document.getElementsByClassName(dotId[no]);
+
+  // If slide n is greater than slide length, then default slide is 1
+  if (n > slides.length) {slideIndex[no] = 1}
+
+  // If slide n is less than slide min length, then default is last slide
+  if (n < 1) {slideIndex[no] = slides.length}
+
+  // For length of slides, set display to none
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  // For length of dots, disable dots
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  // Activate proper slide and dot
+  slides[n-1].style.display = "block";
+  dots[n-1].className += " active";
+}
+
+// let slideIndex = [1,1];
+// let slideId = ["mySlides1", "mySlides2"]
+// showSlides(1, 0);
+// showSlides(1, 1);
+
+// function plusSlides(n, no) {
+//   showSlides(slideIndex[no] += n, no);
+// }
+
+// function showSlides(n, no) {
+//   let i;
+//   let x = document.getElementsByClassName(slideId[no]);
+//   if (n > x.length) {slideIndex[no] = 1}    
+//   if (n < 1) {slideIndex[no] = x.length}
+//   for (i = 0; i < x.length; i++) {
+//      x[i].style.display = "none";  
+//   }
+//   x[slideIndex[no]-1].style.display = "block";  
+// }
+
+
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
