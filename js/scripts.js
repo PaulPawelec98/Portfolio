b13626b591e201f7309584a80d4df4e75c8467ad
SpicyCrabKing <paul_pawelec@hotmail.com>
2025-03-04 17:17:34 -0500

    update
    added a new project from Apziva

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,15 @@
 
 let slideIndex = [1,1];
 
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5"]
-let dotId = ["dots1", "dots2", "dots3", "dots4", "dots5"]
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6"]
+let dotId = ["dots1", "dots2", "dots3", "dots4", "dots5", "dots6"]
 
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
 showSlides(1, 3);
 showSlides(1, 4);
+showSlides(1, 5);
 
 // Next/previous controls
 function plusSlides(n, no) {
